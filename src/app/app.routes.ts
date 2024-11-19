036bd5f641730c639d3687874332155771363f46
EmiSZ98 <julian.suaza39009@ucaldas.edu.co>
2024-11-18 19:10:19 -0500

    Primer commit base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SolarInfoComponent } from './components/solar-info/solar-info.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { ConsumptionCalculatorComponent } from './components/consumption-calculator/consumption-calculator.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'energia-solar', component: SolarInfoComponent },
+  { path: 'datos-historicos', component: DataTableComponent },
+  { path: 'estimador', component: ConsumptionCalculatorComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
