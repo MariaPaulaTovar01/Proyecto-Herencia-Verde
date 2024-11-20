282c48f93ff7f3f2bfc8e6dddacb9414c42bc58a
CrisMontero1 <cristian.montero38972@ucaldas.edu.co>
2024-11-19 21:04:45 -0500

    botones para expandir secciones usando Angular Material

--- src/app/components/solar-info/solar-info.component.spec.ts
+++ src/app/components/solar-info/solar-info.component.spec.ts
@@ -5,6 +5,7 @@ import { SolarInfoComponent } from './solar-info.component';
 describe('SolarInfoComponent', () => {
   let component: SolarInfoComponent;
   let fixture: ComponentFixture<SolarInfoComponent>;
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,4 +21,5 @@ describe('SolarInfoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
 });
--- src/app/components/solar-info/solar-info.component.ts
+++ src/app/components/solar-info/solar-info.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion'; // Importar MatExpansionModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-solar-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatAccordion, MatButtonModule, MatExpansionModule], // Asegúrate de incluir MatExpansionModule
   templateUrl: './solar-info.component.html',
-  styleUrl: './solar-info.component.css'
+  styleUrls: ['./solar-info.component.css'],
 })
 export class SolarInfoComponent {
+  @ViewChild(MatAccordion) accordion: MatAccordion | undefined; // Inicialización del accordion
 
+  expandAll(): void {
+    this.accordion?.openAll(); // Verificar si 'accordion' está definido
+  }
+
+  collapseAll(): void {
+    this.accordion?.closeAll(); // Verificar si 'accordion' está definido
+  }
 }
